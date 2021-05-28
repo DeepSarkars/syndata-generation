2bebee9ca53fef1fbd1335a4d6418b063d14329b
Deepsubhra Sarkar <deepsubhra.sarkar@kgec.edu.in>
2021-05-28 23:39:39 +0530

    slight changes

--- defaults.py
+++ defaults.py
@@ -18,15 +18,15 @@ MIN_NO_OF_OBJECTS = 1
 MAX_NO_OF_OBJECTS = 4
 MIN_NO_OF_DISTRACTOR_OBJECTS = 2
 MAX_NO_OF_DISTRACTOR_OBJECTS = 4
-WIDTH = 224
-HEIGHT = 224
+WIDTH = 448
+HEIGHT = 448
 MAX_ATTEMPTS_TO_SYNTHESIZE = 20
 
 # Parameters for objects in images
 MIN_SCALE = 0.4 # min scale for scale augmentation
-MAX_SCALE = 1.0 # max scale for scale augmentation
+MAX_SCALE = 0.8 # max scale for scale augmentation
 MAX_DEGREES = 30 # max rotation allowed during rotation augmentation
 MAX_TRUNCATION_FRACTION = 0.25 # max fraction to be truncated = MAX_TRUNCACTION_FRACTION*(WIDTH/HEIGHT)
-MAX_ALLOWED_IOU = 0.75 # IOU > MAX_ALLOWED_IOU is considered an occlusion
+MAX_ALLOWED_IOU = 0.50 # IOU > MAX_ALLOWED_IOU is considered an occlusion
 MIN_WIDTH = 6 # Minimum width of object to use for data generation
 MIN_HEIGHT = 6 # Minimum height of object to use for data generation
