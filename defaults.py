f2a068e590a7b187830114af9a661563779bc3aa
Deepsubhra Sarkar <deepsubhra.sarkar@kgec.edu.in>
2021-05-28 22:42:04 +0530

    Update defaults.py

--- defaults.py
+++ defaults.py
@@ -1,30 +1,30 @@
 # Paths
 # Fill this according to own setup
-BACKGROUND_DIR = 'demo_data_dir/backgrounds/'
-BACKGROUND_GLOB_STRING = '*.png'
-POISSON_BLENDING_DIR = '/usr1/debidatd/pb'
-SELECTED_LIST_FILE = 'demo_data_dir/selected.txt'
-DISTRACTOR_LIST_FILE = 'demo_data_dir/neg_list.txt' 
-DISTRACTOR_DIR = 'demo_data_dir/distractor_objects_dir/'
+BACKGROUND_DIR = '/content/drive/MyDrive/datasets/background/farm/'
+BACKGROUND_GLOB_STRING = '*.jpg'
+POISSON_BLENDING_DIR = '/content/pb'
+SELECTED_LIST_FILE = '/content/drive/MyDrive/datasets/pig_data_scripts/selected.txt'
+DISTRACTOR_LIST_FILE = '' 
+DISTRACTOR_DIR = ''
 DISTRACTOR_GLOB_STRING = '*.jpg'
 INVERTED_MASK = True # Set to true if white pixels represent background
 
 # Parameters for generator
 NUMBER_OF_WORKERS = 4
-BLENDING_LIST = ['gaussian','poisson', 'none', 'box', 'motion']
+BLENDING_LIST = ['gaussian', 'none', 'box', 'motion']
 
 # Parameters for images
 MIN_NO_OF_OBJECTS = 1
 MAX_NO_OF_OBJECTS = 4
 MIN_NO_OF_DISTRACTOR_OBJECTS = 2
 MAX_NO_OF_DISTRACTOR_OBJECTS = 4
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 224
+HEIGHT = 224
 MAX_ATTEMPTS_TO_SYNTHESIZE = 20
 
 # Parameters for objects in images
-MIN_SCALE = 0.25 # min scale for scale augmentation
-MAX_SCALE = 0.6 # max scale for scale augmentation
+MIN_SCALE = 0.4 # min scale for scale augmentation
+MAX_SCALE = 1.0 # max scale for scale augmentation
 MAX_DEGREES = 30 # max rotation allowed during rotation augmentation
 MAX_TRUNCATION_FRACTION = 0.25 # max fraction to be truncated = MAX_TRUNCACTION_FRACTION*(WIDTH/HEIGHT)
 MAX_ALLOWED_IOU = 0.75 # IOU > MAX_ALLOWED_IOU is considered an occlusion
